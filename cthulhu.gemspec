a0640b811f991a0ef01c3330758bdb6457228134
Jack Ellis <andellis@amazon.com>
2014-06-21 16:47:01 -0700

    Creating the first gem

--- cthulhu.gemspec
+++ cthulhu.gemspec
@@ -20,4 +20,9 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_runtime_dependency "highline/import"
+  spec.add_runtime_dependency "aws-sdk"
+  spec.add_runtime_dependency "terminal-table"
+
 end
+
--- lib/cthulhu.rb
+++ lib/cthulhu.rb
@@ -1,5 +1,18 @@
-require "cthulhu/version"
+#! /usr/bin/env ruby
 
-module Cthulhu
-  # Your code goes here...
-end
+require 'rubygems'
+require 'highline/import'
+# require 'aws-sdk'
+# require 'terminal-table'
+require '../lib/configuration'
+require '../lib/base_methods'
+require '../lib/questions'
+
+# Sets the Access Key, Secret key, and Keypair path
+configuration = StartingConfiguration.config
+
+BaseMethods.start_over("none")
+
+#TODO Add Elastic Beanstalk Management
+#TODO Terminate all "Daily" instances
+#TODO Remove SSH if keypair is not given
